b15e8ac276831a0efdc2758e1302bf3c2306f85d
mickedance <mickedance@hotmail.com>
2022-03-30 08:52:44 +0200

    First commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,63 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+/*
+* 1
+
+2
+
+Working with Strings
+
+
+
+1. What is the length of the String: "Java" ?
+
+1. What char is at index position 6 in the following String:
+"Long example sentence" ?
+
+2. What is the index position of 'o' in the following String:
+"Even longer example sentence" ?
+
+3. Given the following String: "Ok this is not as long!"
+create a substring of only "not as long" (excluding the
+exclamation point) and print it out.
+
+4. Convert the following String: "CAPS EQUALS SCREAMING" to
+lowercase and print it out. Then convert it back to
+uppercase and print it out again.
+
+5. Correct the following String: "Java is the worst
+programming language!" by replacing the (obviously
+incorrect) word "worst" with the word "best". Then print
+out the sentence.
+
+6. What is the output of the following String:
+"\tJ\ta\tv\ta\t" after you trim it?
+
+7. Parse the following int: 20 to a String and add a 20 to the
+end of the String. Printing it out should return: "2020".
+
+8. Oil and water don't go well together. Given the String:
+"Oil and Water", split them up into the words "Oil","Water"
+and store them in a String array.
+
+9. Split the following String: "Carl,Susie,Fredrick,Bob,Erik"
+into an array. Print out all names separately.
+
+10. Convert the following String: "ThisShouldBeConverted" to
+a char array. Iterate through the char array and print out
+each element.
+
+11. Convert the following char[]: {'J','a','v','a'} to a
+String and print it out.
+ */
---
+++ src/main/java/org/example/Ex1.java
@@ -0,0 +1,11 @@
+package org.example;
+/*1. What is the length of the String: "Java" ? */
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+        String s = "Java";
+
+        System.out.println(s.length());
+    }
+}
